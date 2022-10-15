9112f06271ee26fdd7e4108ee28ca7d28c447bbd
Thomas Sieverding <Thomas@bajix.com>
2022-10-15 06:49:42 -0700

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# bit-bounds
-Helper traits for const generic binary bounds 
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+[![Cargo](https://img.shields.io/crates/v/bit-bounds.svg)](https://crates.io/crates/bit-bounds)
+[![Documentation](https://docs.rs/bit-bounds/badge.svg)](https://docs.rs/bit-bounds)
+
+Helper traits for const generic bitwise bounds
+
+```rust
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+
+use bit_bounds::{IsPowerOf2, usize::*};
+
+struct Buffer<const N: usize> {
+  inner: [usize; N],
+}
+
+impl<const N: usize> Buffer<N>
+where
+  Int<N>: IsPowerOf2
+{
+  pub const fn new() -> Self {
+    Buffer { inner: [0; N] }
+  }
+}
+
+fn extract_index<const N: usize>(counter: usize) -> usize
+where
+  Int<N>: IsPowerOf2,
+  Int<N>: BitsAllClear<{ (u32::MAX as usize) << 32 }>,
+{
+  (counter >> 32) & (N - 1)
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+//!
+//! Helper traits for const generic bounds
+//!
+//! ```rust
+//! #![allow(incomplete_features)]
+//! #![feature(generic_const_exprs)]
+//!
+//! use bit_bounds::{IsPowerOf2, usize::*};
+//!
+//! struct Buffer<const N: usize> {
+//!   inner: [usize; N],
+//! }
+//!
+//! impl<const N: usize> Buffer<N>
+//! where
+//!   Int<N>: IsPowerOf2
+//! {
+//!   pub const fn new() -> Self {
+//!     Buffer { inner: [0; N] }
+//!   }
+//! }
+//!
+//! fn extract_index<const N: usize>(counter: usize) -> usize
+//! where
+//!   Int<N>: IsPowerOf2,
+//!   Int<N>: BitsAllClear<{ (u32::MAX as usize) << 32 }>,
+//! {
+//!   (counter >> 32) & (N - 1)
+//! }
+//! ```
+
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+use const_assert::{Assert, IsTrue};
+
+pub trait IsPowerOf2 {}
+
+macro_rules! define_bit_bounds {
+  ($ty:ident) => {
+    pub mod $ty {
+      use super::*;
+      pub trait BitsAllSet<const M: $ty> {}
+      pub trait BitsAnySet<const M: $ty> {}
+      pub trait BitsAllClear<const M: $ty> {}
+      pub trait BitsAnyClear<const M: $ty> {}
+
+      pub struct Int<const N: $ty>;
+      impl<const N: $ty> IsPowerOf2 for Int<N> where Assert<{ N == N.next_power_of_two() }>: IsTrue {}
+      impl<const N: $ty, const M: $ty> BitsAllSet<M> for Int<N> where Assert<{ N & M == M }>: IsTrue {}
+      impl<const N: $ty, const M: $ty> BitsAnySet<M> for Int<N> where Assert<{ N & M != 0 }>: IsTrue {}
+      impl<const N: $ty, const M: $ty> BitsAllClear<M> for Int<N> where Assert<{ (N ^ M) & M == M }>: IsTrue {}
+      impl<const N: $ty, const M: $ty> BitsAnyClear<M> for Int<N> where Assert<{ N & M != M }>: IsTrue {}
+    }
+  };
+}
+
+define_bit_bounds!(u8);
+define_bit_bounds!(u16);
+define_bit_bounds!(u32);
+define_bit_bounds!(u64);
+define_bit_bounds!(u128);
+define_bit_bounds!(usize);
